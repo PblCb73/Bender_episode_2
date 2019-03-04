a31d2d5a8a9ce5a2c0122865f483b06dcb2abbda
test <test@test.com>
2019-03-04 21:34:04 +0400

    Combed the code and added comments

--- main.cpp
+++ main.cpp
@@ -3,44 +3,38 @@
 #include <sstream>
 #include <iterator>
 #include <vector>
-#include <array>
 #include <algorithm>
 
 using namespace std;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
+//Класс узла
 class Node
 {
 public:
-    void setParam( Node* NL, Node* NR, int val){
-         nL = NL;
-         nR = NR;
-         value = val;
-         tempValue = val; 
-    }
-    
-    void setNextValue( int v  )
+    Node():_value(0),_tempValue(0),_hasParent(false){}
+	//Инициализация узла
+    void initValue( int value ){ _value = value; }
+	//Установка текущего значения в узле
+    void setCurrentValue( int value, bool hasParent )
     {
-        if( value + v > tempValue ) 
-        {
-            tempValue = value + v;
-            //value = 0;
-        }
+        _hasParent = hasParent;
+		/* Если узел не имеет предка, то мы в этот узел не попадем, 
+		   а следовательно, что бы исключит путь из поиска максимального значения 
+		   _tempValue будет равно 0 */
+        if( _hasParent && value > _tempValue ) _tempValue = value;
     }
+	//Получение текущего значения в узле
+    int getCurrentValue(){ return _tempValue + _value; }
+	//Получения флага того, что узел имеет предка
+    bool hasParent(){ return _hasParent; }
         
-public:
-    Node *nL;
-    Node *nR;
-    int value;
-    int tempValue;
-    bool hasParent;
+private:
+    int _value;
+    int _tempValue;
+    bool _hasParent;
 };
 
-
+//Функция для разбиения входной строки на подстроки
 vector<string> split(string str)
 {
     std::istringstream iss(str);
@@ -48,93 +42,25 @@ vector<string> split(string str)
     return result;
 }
 
-
 int main()
 {
     int N;
     cin >> N; cin.ignore();
-    vector<Node*> nodes;
-    vector<Node*> exitNodes;
-    for( int i = 0 ; i<N ; ++i )
-    {
-        nodes.push_back( new Node() );
-    }
-    
-    for (int i( 0 ); i < N; ++i )
-    {
+	//Тут хранятся все узлы
+    vector<Node> nodes(N);
+    //Будем плюсовать текущее значение в узле к значению следующих дочерних узлов,
+	//на выходах получим максимальное значение
+    for( int i( 0 ); i < N; ++i ){
         string item;
         getline( cin, item );
-        vector<string> sss = split( item );
-        cerr << item << endl;
-        Node* nodeL( NULL );
-        Node* nodeR( NULL );
-        if( !nodes[ i ]->hasParent )
-        {
-            cerr<<"dont has parents:"<<i<<endl;
-        }
-        if( sss[ 2 ] != "E" )
-        {
-            nodeL = nodes[ stoi( sss[ 2 ] ) ];
-            nodeL->hasParent = true;
-            //nodeL->setNextValue(nodes[i]->tempValue);
-        }
-        if( sss[ 3 ] != "E" ) 
-        {
-            nodeR = nodes[ stoi( sss[ 3 ] ) ];
-            nodeR->hasParent = true;
-            //nodeR->setNextValue(nodes[i]->tempValue);
-        }
-        if( sss[ 2 ] == "E" or sss[ 3 ] == "E"  )
-        {
-            exitNodes.push_back( nodes[ i ] );
-        }
-        nodes[ i ]->setParam( nodeL, nodeR, stoi( sss[ 1 ] ) );
+        vector<string> params( split( item ) );
+        nodes[ i ].initValue( stoi( params[ 1 ] ) );
+        if( params[ 2 ] != "E" ) nodes[ stoi( params[ 2 ] ) ].setCurrentValue( nodes[ i ].getCurrentValue(), nodes[ i ].hasParent() || i==0 ); 
+        if( params[ 3 ] != "E" ) nodes[ stoi( params[ 3 ] ) ].setCurrentValue( nodes[ i ].getCurrentValue(), nodes[ i ].hasParent() || i==0 );
     }
-    
-    
-    
-    
-    
-    
-    if( nodes[ 0 ]->nL != NULL )
-    {
-        nodes[ 0 ]->nL->setNextValue( nodes[ 0 ]->tempValue );// += nodes[i]->value;
-    }
-    if( nodes[ 0 ]->nR != NULL )
-    {
-        nodes[ 0 ]->nR->setNextValue( nodes[ 0 ]->tempValue );
-    }
-        
-    for( int i = 1 ; i < nodes.size() ; i++ )
-    {
-        
-        if( nodes[ i ]->nL != NULL )
-        {
-            nodes[ i ]->nL->hasParent = nodes[ i ]->hasParent;
-            if( nodes[ i ]->nL->hasParent )
-            {
-                nodes[ i ]->nL->setNextValue( nodes[ i ]->tempValue );// += nodes[i]->value;
-            }
-        }
-        if( nodes[ i ]->nR != NULL )
-        {
-            nodes[ i ]->nR->hasParent = nodes[ i ]->hasParent;
-            if( nodes[ i ]->nR->hasParent )
-            {
-                nodes[ i ]->nR->setNextValue( nodes[ i ]->tempValue );
-            }
-        }
-    }
-    
-    int summ = 0;
-    for( int i(0) ; i<exitNodes.size() ; i++ )
-    {
-        if(summ<exitNodes[i]->tempValue)
-        {
-            summ = exitNodes[i]->tempValue;
-        }
-        cerr<<"  "<<exitNodes[i]->hasParent<<"  "<<exitNodes[i]->tempValue<<endl;
-    }
-    cout << summ << endl;
-    
+	//найдем максимальное значение, оно и будет решением
+    vector<Node>::iterator itNode = max_element( nodes.begin(), nodes.end(), []( Node &lN, Node &rN ){ 
+        return lN.getCurrentValue() < rN.getCurrentValue();
+    } );
+    cout<<(*itNode).getCurrentValue()<<endl;
 }
